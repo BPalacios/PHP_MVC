926dd591ed613b3edbba0fee9060fd134d805007
BPalacios <alex.palacios@unah.hn>
2019-07-31 07:25:52 -0600

    Cambio en entidad Base

--- core/EntidadBase.php
+++ core/EntidadBase.php
@@ -4,6 +4,14 @@ class EntidadBase{
     private $db;
     private $conectar;
 
+    public function __construct($table)
+    {
+        $this -> table = (string) $table;
+        require_once 'Conectar.php';
+        $this -> conectar = new Conectar();
+        $this -> db = $this -> conectar -> conexion();
+
+    }
     
 }
 ?>
